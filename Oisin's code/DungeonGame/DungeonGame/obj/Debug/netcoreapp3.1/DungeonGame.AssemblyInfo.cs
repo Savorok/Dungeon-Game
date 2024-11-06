b4c0934ae57a0af73c58313f7de176f09b920ddd
savorok <oisincassidydawson179@gmail.com>
2024-11-06 09:08:55 +0000

    C# stuff

--- Oisin's code/DungeonGame/DungeonGame/obj/Debug/netcoreapp3.1/DungeonGame.AssemblyInfo.cs
+++ Oisin's code/DungeonGame/DungeonGame/obj/Debug/netcoreapp3.1/DungeonGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3b18cbdb08585fbb7c6457ad92dc0c49e689d62")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
